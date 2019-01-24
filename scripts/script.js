d0bb1f43641ce26ba90143a1160b0e46cbfbad29
thepam <pamela.rdom@gmail.com>
2019-01-23 23:14:48 -0800

    optimizinf the script file

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//Main listing methods
+
 function filterGenre(val){
 	var filter = genresArray[val];
 	var listed = document.getElementsByClassName('book');
@@ -53,39 +55,119 @@ function createStars(rating){
 	return response;
 }
 
-document.getElementById("menuarrow").onclick = function(){
-	var menu = document.getElementById("genre-list");
-	if(menu.style.display == "block"){
-		menu.style.display = "none";
-	}
-	else{
-		menu.style.display = "block";
+function setResponsiveMenu(){
+	document.getElementById("menuarrow").onclick = function(){
+		var menu = document.getElementById("genre-list");
+		if(menu.style.display == "block"){
+			menu.style.display = "none";
+		}
+		else{
+			menu.style.display = "block";
+		}
 	}
 }
 
-var code = "";
-var genresArray = [];
-var books = JSON.parse(obj);
-books.forEach( function(book) {
-	code = code + "<div id='"+book['id']+"' class='book card-panel'>";
-	code = code + "<div class='cover'><img src='images/"+book['id']+".jpg'/></div>"
-	code = code + "<div class='info'><p class='title'><a class='teal-text' href='item.html?book="+book['id']+"'>"+book['name']+"</a></p>";
-	code = code + "<p class='authoretc'>Written by "+book['author']+" in "+ book['year'] +" &nbsp;-&nbsp; "+createStars(book['rating'])+"</p>";
-	code = code + "<p class='summary'>"+book['summary']+"</p>";
-	code = code + "<p class='genres blue-text text-darken-2'>";
-	book['genres'].forEach( function (genre) {
-		code = code + genre['name'] + ", ";
-		if(genresArray.indexOf(genre['name']) == -1){
-			genresArray.push(genre['name']);
+function setContentList(books){
+	var genresArray = [];
+
+	books.forEach( function(book) {
+		code = code + "<div id='"+book['id']+"' class='book card-panel'>";
+		code = code + "<div class='cover'><img src='images/"+book['id']+".jpg'/></div>"
+		code = code + "<div class='info'><p class='title'><a class='teal-text' href='item.html?book="+book['id']+"'>"+book['name']+"</a></p>";
+		code = code + "<p class='authoretc'>Written by "+book['author']+" in "+ book['year'] +" &nbsp;-&nbsp; "+createStars(book['rating'])+"</p>";
+		code = code + "<p class='summary'>"+book['summary']+"</p>";
+		code = code + "<p class='genres blue-text text-darken-2'>";
+		book['genres'].forEach( function (genre) {
+			code = code + genre['name'] + ", ";
+			if(genresArray.indexOf(genre['name']) == -1){
+				genresArray.push(genre['name']);
+			}
+		});
+		code = code.slice(0,-2);
+		code = code + "</p>";
+		code = code + "</div>";
+		code = code + "</div>";
+	});
+
+	document.getElementById("content-list").innerHTML = code;
+	genresArray = genresArray.sort();
+	document.getElementById("genre-list").innerHTML = createMenu(genresArray);
+}
+
+//Individual book page methods
+
+function getURLVariable(variable){
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){
+        	return pair[1];
+        }
+    }
+    return(false);
+}
+
+function friendlyDate(bookDate){
+	var myDate = new Date(bookDate);
+	var month = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"][myDate.getMonth()];
+	return month + ' ' + myDate.getDay() + ', ' + myDate.getFullYear();
+}
+
+function setContentBook(books){
+	var itemID = getURLVariable("book");
+	books.forEach( function(book) {
+		if(book['id'] == itemID){
+			document.getElementById("book-crumb").innerHTML = book['name'];
+
+			code = "<article class='card-panel'>";
+			code = code + "<h1>"+book['name']+"</h1>";
+			if(book['series'] !== ""){
+				code = code + "<h3>"+book['series']+"</h3>";
+			}
+			code = code + "<h4>Written by "+book['author']+" in "+book['year']+"</h4>";
+			code = code + "<p id='book-ratings'><span class='stars'>"+createStars(book['rating'])+"</span><span class='ratings'>("+book['numratings']+" ratings)</span></p>";
+			code = code + "<div class='book-desc'><p>"+book['description'].replace(/(\r\n|\n\r|\r|\n)/gm, "</p><p>")+"</p></div>";
+			code = code + "</article>";
+			code = code + "<aside class='card-panel'><h2>Reviews</h2>";
+			book['reviews'].forEach( function (review) {
+				code = code + "<div class='comment row'><div class='comment-icon col s1'><i class='fa fa-user-circle-o' aria-hidden='true'></i></div>";
+				code = code + "<div class='col s11'>";
+				code = code + "<p class='comment-name'>"+review['name']+" <span>"+createStars(review['rating'])+"</span></p>";
+				code = code + "<p class='comment-date'>"+friendlyDate(review['date'])+"</p>";
+				code = code + "<div class='comment-text'><p>"+review['text'].replace(/(\r\n|\n\r|\r|\n)/gm, "</p><p>")+"</p></div>";
+				code = code + "</div></div>";
+			});
+			code = code + "</aside>";
+
+
+			document.getElementById("content-list").innerHTML = code;
+
+			code = "<p>"+book['about-author'].replace(/(\r\n|\n\r|\r|\n)/gm, "</p><p>")+"</p>";
+			code = code + "<h5 class='menutitle teal-text'>About this Edition</h5>";
+			code = code + "<div class='row'><div class='col s4 m3 l4'><img src='images/"+book['id']+".jpg'/></div>";
+			code = code + "<div class='col s8 m9 l8'>";
+			code = code + "<p>Published on "+friendlyDate(book['published'])+" by "+book['publisher']+"<p>";
+			code = code + "<p><em class='caps'>"+book['edition']+"</em>, "+book['pages']+" pages</p>";
+			code = code + "<p>"+book['numeditions']+" editions</p>";
+			code = code + "</div></div>";
+
+			document.getElementById("author-info").innerHTML = code;
 		}
 	});
-	code = code.slice(0,-2);
-	code = code + "</p>";
-	code = code + "</div>";
-	code = code + "</div>";
-});
+}
 
-document.getElementById("content-list").innerHTML = code;
-genresArray = genresArray.sort();
-document.getElementById("genre-list").innerHTML = createMenu(genresArray);
+//Run on page load
+
+var code = "";
+var books = JSON.parse(obj);
+if(getURLVariable("book")){
+	setContentBook(books);
+}else{
+	setResponsiveMenu();
+
+	setContentList(books);
+}
+
 
